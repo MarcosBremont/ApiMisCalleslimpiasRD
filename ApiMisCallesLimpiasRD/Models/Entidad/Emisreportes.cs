55d5ef7bafc39b77f4bec2a420d4976966698c98
emmanuelbeltre <beltrelgg2@gmail.com>
2021-08-08 17:29:59 -0400

    cambios en metodos de reportes por empresas

--- ApiMisCallesLimpiasRD/Models/Entidad/Emisreportes.cs
+++ ApiMisCallesLimpiasRD/Models/Entidad/Emisreportes.cs
@@ -8,13 +8,18 @@ namespace ApiMisCallesLimpiasRD.Models.Entidad
 {
   public class Emisreportes
   {
-    public int cod_reporte { get; set; } = 0;
-    public string ubicacion { get; set; } = "";
-    public string lat { get; set; } = "";
-    public string lng { get; set; } = "";
-    public string fotos { get; set; } = "";
-    public int cod_usuario { get; set; } = 0;
-    public string respuesta { get; set; }
-    public string mensaje { get; set; }
+        public int cod_reporte { get; set; } = 0;
+        public string ubicacion { get; set; } = "";
+        public string lat { get; set; } = "";
+        public string lng { get; set; } = "";
+        public string fotos { get; set; } = "";
+        public int cod_usuario { get; set; } = 0;
+        public int cod_ayuntamiento { get; set; } = 0;
+        public string usuario { get; set; } = "";
+        public string correo_Usuario { get; set; } = "";
+        public string cedula_usuario { get; set; } = ""; 
+        public string telefono_Usuario { get; set; } = "";
+        public string respuesta { get; set; }
+     public string mensaje { get; set; }
   }
 }
